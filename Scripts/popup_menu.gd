23825fcb3f3333d9e712332536c565bf4a316309
HeyBongga <herzogfa@hs-pforzheim.de>
2025-11-13 16:48:47 +0100

    Popupmenu_implementiert

---
+++ Scripts/popup_menu.gd
@@ -0,0 +1,37 @@
+extends MarginContainer
+
+# Referenzen zu den Buttons
+@onready var toggle_menu_button = $ToggleContainer/togglemenubutton
+@onready var options_button = $PopupContainer/OptionsContainer/optionsbutton
+@onready var quit_button = $PopupContainer/QuitContainer/quitbutton
+
+var menu_visible = false  # Merkt sich, ob Menü gerade offen ist
+
+func _ready():
+	# Menü-Buttons beim Start ausblenden
+	options_button.visible = false
+	quit_button.visible = false
+
+	# Signal verbinden
+	toggle_menu_button.pressed.connect(_on_togglemenubutton_pressed)
+
+func _on_togglemenubutton_pressed():
+	# Sichtbarkeit umschalten
+	menu_visible = !menu_visible
+
+	options_button.visible = menu_visible
+	quit_button.visible = menu_visible
+
+#@export var toggle_screen: MarginContainer
+#@export var popup_screen: MarginContainer
+#
+#func toggle_visibility(object):
+	#if object.visible:
+		#object.visible = false
+	#else:
+		#object.visible = true
+#
+#
+#func _on_togglemenubutton_pressed():
+	#toggle_visibility(toggle_screen)
+	#toggle_visibility(popup_screen)
