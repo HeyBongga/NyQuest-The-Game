4e7bf378ee9aff3767324732f798dd391a60f3e0
HeyBongga <herzogfa@hs-pforzheim.de>
2025-11-19 16:49:21 +0100

    World/Island2/windrad
    Anpassungen

--- Scripts/island_tile_2.gd
+++ Scripts/island_tile_2.gd
@@ -1,4 +1,4 @@
 extends Node2D
 
 func _ready() -> void:
-	$".".visible = false
+	self.visible = false
--- Scripts/windrad.gd
+++ Scripts/windrad.gd
@@ -1,19 +1,23 @@
 extends Area2D
 
 @export var objectType := "windrad"
+
 signal clicked(this_object)
 
-@onready var sprite := $Sprite2D
+@onready var animatedSprite := $AnimatedSprite2D
+
+
 var zoom_scale := Vector2(1.2, 1.2)  # Zielgröße beim Hover
 
 func _ready():
 	print("Windrad ready – warte auf Input")
+	animatedSprite.play("Windrad_idle")
 
 func on_mouse_entered():
-	sprite.scale = zoom_scale
+	animatedSprite.scale = zoom_scale
 
 func on_mouse_exited():
-	sprite.scale = Vector2.ONE
+	animatedSprite.scale = Vector2.ONE
 
 func on_input_event(_viewport: Viewport, event: InputEvent, _shape_idx: int):
 	if event.is_action_pressed("click_left_mouse"):
